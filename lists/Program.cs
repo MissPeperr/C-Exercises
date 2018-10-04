495ba20892f90cb195033e01eb93e1583976dee7
MissPeperr <madisonpeper@gmail.com>
2018-10-04 11:10:07 -0500

    finished lists and sets exercises

---
+++ lists/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists {
+    class Program {
+        static void Main (string[] args) {
+            // created a new list called "planetList", contains two planets
+            List<string> planetList = new List<string> () { "Mercury", "Mars" };
+
+            // add two new planets into the "planetList" List
+            planetList.Add ("Jupiter");
+            planetList.Add ("Saturn");
+
+            // created a new list called "lastPlanets", contains two planets
+            List<string> lastPlanets = new List<string> () { "Neptune", "Uranus" };
+
+            // added the entire list of "lastPlanets" to the list "planetList"
+            planetList.AddRange(lastPlanets);
+
+            /* Inserted two planets at two different indexes, added "Pluto" at the very end of the List, then removed it because Pluto isn't actually a planet :(
+            */
+            planetList.Insert (1, "Venus");
+            planetList.Insert (2, "Earth");
+            planetList.Add ("Pluto");
+            planetList.Remove ("Pluto");
+
+            // Created a new list called "rockyPlanets" that has the planets that are made out of rock from "planetList"
+            List<string> rockyPlanets = planetList.GetRange (0, 4);
+            // testing if planets were added to "rockyPlanets"
+            rockyPlanets.ForEach (planet => Console.WriteLine ($"Rocky Planets: {planet}"));
+
+            // Created a new list, that contains dictionaries called "allProbes"
+            List<Dictionary<string, string>> allProbes = new List<Dictionary<string, string>> () {
+                new Dictionary<string, string> () {{ "Venus", "Venus 1" }},
+                new Dictionary<string, string> () {{ "Mars", "Mariner 3" }},
+                new Dictionary<string, string> () {{ "Jupiter", "Juno" }},
+                new Dictionary<string, string> () {{ "Saturn", "Pioneer 11" }}
+            };
+
+            // loop over the "planetList" List
+            foreach (string planet in planetList) // iterate planets
+            {
+                // created an empty list called "matchingProbes"
+                List<string> matchingProbes = new List<string> ();
+
+                // loop over the "allProbes" List
+                foreach (Dictionary<string, string> probe in allProbes) // iterate probes
+                {
+                    /*
+                        Does the current Dictionary contain the key of
+                        the current planet? Investigate the ContainsKey(planet)
+                        method on a Dictionary.
+
+                        If so, add the current spacecraft to `matchingProbes`.
+                    */
+                        if(probe.ContainsKey(planet)){
+
+                            matchingProbes.Add(probe[planet]);
+                        }
+                }
+
+                /*
+                    Use String.Join(",", matchingProbes) as part of the
+                    solution to get the output below. It's the C# way of
+                    writing `array.join(",")` in JavaScript.
+                */
+                // string completedList = string.Join(", ", matchingProbes);
+                Console.WriteLine($"{planet}: {String.Join(", ", matchingProbes)}");
+            }
+        }
+    }
+}
---
+++ sets/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // created an empty HashSet called "Showroom"
+            HashSet<string> Showroom = new HashSet<string>();
+
+            // added 4 cars to "Showroom"
+            Showroom.Add("Model 3");
+            Showroom.Add("4Runner");
+            Showroom.Add("Fusion");
+            Showroom.Add("Wrangler");
+            /* 
+             added "Model 3" again to show that HashSets have UNIQUE items, so Model 3 will only appear once in the console
+            */
+            Showroom.Add("Model 3");
+
+
+            // created an empty HashSet called "UsedLot"
+            HashSet<string> UsedLot = new HashSet<string>();
+
+            // added two cars to "UsedLot"
+            UsedLot.Add("Fiesta");
+            UsedLot.Add("F350");
+            
+            // added the HashSet "UsedLot" to the HashSet "Showroom"
+            Showroom.UnionWith(UsedLot);
+
+            // removing 1 car from the "Showroom" HashSet
+            Showroom.Remove("Fusion");
+
+            // printing to console to make sure cars were added
+            Console.WriteLine("///// Showroom Console /////");
+            foreach(string vehicle in Showroom)
+            {
+                Console.WriteLine(vehicle);
+            }
+
+            // created an empty HashSet called "Junkyard"
+            HashSet<string> Junkyard = new HashSet<string>();
+
+            // added 4 cars to the HashSet "Junkyard"
+            Junkyard.Add("Camry");
+            Junkyard.Add("Sonata");
+            Junkyard.Add("Prius");
+            Junkyard.Add("Taurus");
+            // added 2 cars that are named the same from "Showroom" to "Junkyard"
+            Junkyard.Add("Model 3");
+            Junkyard.Add("4Runner");
+
+            // created a new "Showroom" HashSet
+            HashSet<string> clone = new HashSet<string>(Showroom);
+            
+            // checking to see if any vehicles in the clone of "Showroom" match in "Junkyard"
+            clone.IntersectWith(Junkyard);
+
+            // looping over the clone HashSet to see the matching vehicles
+            Console.WriteLine("///// Clone Console /////");
+            foreach(string vehicle in clone)
+            {
+                Console.WriteLine(vehicle);
+            }
+
+            // added HashSet "Junkyard" to HashSet "Showroom"
+            Showroom.UnionWith(Junkyard);
+
+            // printing to console to make sure cars were added
+            Console.WriteLine("///// Added Junkyard to Showroom Console ///// ");
+            foreach(string vehicle in Showroom)
+            {
+                Console.WriteLine(vehicle);
+            }
+
+        }
+    }
+}
